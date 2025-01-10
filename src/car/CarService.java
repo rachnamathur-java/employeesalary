ed96a71df5a433d457806cf14093d14df4c79ab9
Rachna Mathur <rachnamathur.ds@gmail.com>
2025-01-10 12:06:45 -0500

    some output display modified

--- src/car/CarService.java
+++ src/car/CarService.java
@@ -12,31 +12,40 @@ public class CarService {
 
 
     public void getDetails(){
-
+        boolean flag = false;
+        while (!flag){
         Utils.choosecar();
         int choice = scanner.nextInt();
 
-        switch (choice){
-            case 1:
-                System.out.println("Diesel Car details...");
-                System.out.println();
-                diesel.printDetails("BMW", "SUV",53456.00);
-                break;
+            switch (choice) {
+                case 1:
+                    System.out.println("Diesel Car details...");
+                    System.out.println();
+                    diesel.printDetails("BMW", "SUV", 53456.00);
+                    System.exit(0);
+                    break;
 
-            case 2:
-                System.out.println("Electric Car details...");
-                System.out.println();
-                electric.printDetails("TOYOTA", "sedan", 52674.00);
-                break;
+                case 2:
+                    System.out.println("Electric Car details...");
+                    System.out.println();
+                    electric.printDetails("TOYOTA", "sedan", 52674.00);
+                    System.exit(0);
+                    break;
 
-            case 3:
-                System.out.println("Gasoline Car details...");
-                System.out.println();
-                gasoline.printDetails("HONDA", "SUV", 56754.00);
-                break;
+                case 3:
+                    System.out.println("Gasoline Car details...");
+                    System.out.println();
+                    gasoline.printDetails("HONDA", "SUV", 56754.00);
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Please enter a valid choice...");
+                    System.out.println("try again...");
+
+            }
 
         }
-
     }
 
 }
